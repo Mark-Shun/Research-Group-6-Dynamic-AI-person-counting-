2f8547b8947123539fa499fc182b4d02b914d836
Nic Jenneboer <nicjenneboer@gmail.com>
2023-05-17 08:09:51 +0200

    Fix - saving segmentation wihout showing video

--- track.py
+++ track.py
@@ -267,7 +267,7 @@ def run(
                                 f.write(('%g ' * 10 + '\n') % (frame_idx + 1, id, bbox_left,  # MOT format
                                                                bbox_top, bbox_w, bbox_h, -1, -1, -1, i))
 
-                        if save_vid or save_crop or show_vid:  # Add bbox/seg to image
+                        if save_vid or save_crop or show_vid or (is_seg and save_seg):  # Add bbox/seg to image
                             c = int(cls)  # integer class
                             id = int(id)  # integer id
                             label = None if hide_labels else (f'{id} {names[c]}' if hide_conf else \
