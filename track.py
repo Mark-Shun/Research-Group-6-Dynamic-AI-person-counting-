7132d2e31a15ec1e53bdb9dbb53ca2bd29c1dbdb
Nic Jenneboer <nicjenneboer@gmail.com>
2023-05-17 08:13:26 +0200

    Save image segmentation in different folder than image crop

--- track.py
+++ track.py
@@ -280,9 +280,9 @@ def run(
                                 tracker_list[i].trajectory(im0, q, color=color)
                             if save_crop:
                                 txt_file_name = txt_file_name if (isinstance(path, list) and len(path) > 1) else ''
-                                save_one_box(np.array(bbox, dtype=np.int16), imc, file=save_dir / 'crops' / txt_file_name / names[c] / f'{id}' / f'{p.stem}.jpg', BGR=True)
+                                save_one_box(np.array(bbox, dtype=np.int16), imc, file=save_dir / 'crops-img' / txt_file_name / names[c] / f'{id}' / f'{p.stem}.jpg', BGR=True)
                             if save_seg:
-                                save_one_box(np.array(bbox, dtype=np.int16), segIMG.astype(np.uint8), file=save_dir / 'crops' / txt_file_name / names[c] / f'{id}' / f'{p.stem}-seg.jpg', BGR=False)
+                                save_one_box(np.array(bbox, dtype=np.int16), segIMG.astype(np.uint8), file=save_dir / 'crops-seg' / txt_file_name / names[c] / f'{id}' / f'{p.stem}-seg.jpg', BGR=False)
 
             else:
                 pass
