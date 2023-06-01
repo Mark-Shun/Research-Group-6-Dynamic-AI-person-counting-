32130b6faa282ebe1b3f40def34eaddb7082dade
Mark-Shun <57092443+Mark-Shun@users.noreply.github.com>
2023-06-01 15:48:56 +0200

    Added new test images and extended stitching lib python script.

--- stitching_lib.py
+++ stitching_lib.py
@@ -1,10 +1,24 @@
+import os
 import stitching
 import cv2
+import time
 
-settings = {"detector": "sift", "confidence_threshold": 0.2, "nfeatures" : 500}
+settings = {"detector": "sift", "confidence_threshold": 0.2, "nfeatures" : 1000, "try_use_gpu" : True}
 stitcher = stitching.Stitcher(**settings)
+input_images = []
+directory = 'images/backyard2/'
 
-panorama = stitcher.stitch(["./images/image1.jpg", "./images/image2.jpg", "./images/image3.jpg", "./images/image4.jpg", "./images/image5.jpg"])
+for file in os.listdir(directory):
+    if(file != "stitched_image.jpg"):
+        f = os.path.join(directory, file)
+        if os.path.isfile(f):
+            input_images.append(f)
 
-cv2.imwrite("./images/stitched_image.jpg", panorama)
+start_time = time.time()
+panorama = stitcher.stitch(input_images)
+end_time = time.time()
+elapsed_time = end_time - start_time
+
+print(f"Finished stitching directory: {directory}.\n Processed {len(input_images)} images, which took: {elapsed_time} seconds.")
+cv2.imwrite(f"{directory}stitched_image.jpg", panorama)
 cv2.waitKey(0)
