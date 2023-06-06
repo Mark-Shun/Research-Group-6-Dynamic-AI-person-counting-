347730c63d80f0b0e6cafbbb499c7d8cad15f7d9
nick teeuwen <nickteeuwen21@gmail.com>
2023-06-06 13:22:22 +0200

    read out of dir

--- test.py
+++ test.py
@@ -14,40 +14,41 @@ average_img1 = 0
 # average_img3 = 0
 # average_img4 = 0
 
-img = cv2.imread("img/nic1.jpg")
+img = cv2.imread("img/nic/nic1.jpg")
+for d in os.listdir("C:/Users/nickt/Desktop/Research/img"):
+    print(d)
+    for images in os.listdir(f"C:/Users/nickt/Desktop/Research/img/{d}"):
+        print(f"{d}/{images}")
+        img2 = cv2.imread(f"C:/Users/nickt/Desktop/Research/img/{d}/{images}")
 
-for images in os.listdir("C:/Users/nickt/Desktop/Research/img"):
-    print(f"img/{images}")
-    img2 = cv2.imread(f"img/{images}")
-
-    average_img1 = 0
-    test = hog(
-        img,
+        average_img1 = 0
+        test = hog(
+            img,
+            orientations=8,
+            channel_axis=-1,
+        )
+        test2 = hog(
+        img2,
         orientations=8,
         channel_axis=-1,
-    )
-    test2 = hog(
-    img2,
-    orientations=8,
-    channel_axis=-1,
-    )
+        )
 
-    colors = ("b", "g", "r")
-    for i, color in enumerate(colors):
-        hist = cv2.calcHist([img], [i], None, [256], [0, 256])
-        hist[0] = 0
+        colors = ("b", "g", "r")
+        for i, color in enumerate(colors):
+            hist = cv2.calcHist([img], [i], None, [256], [0, 256])
+            hist[0] = 0
 
-        hist2 = cv2.calcHist([img2], [i], None, [256], [0, 256])
-        hist2[0] = 0
+            hist2 = cv2.calcHist([img2], [i], None, [256], [0, 256])
+            hist2[0] = 0
 
-        base_base = cv2.compareHist(hist, hist, 0)
-        base_test1 = cv2.compareHist(hist, hist2, 0)
-        average_img1 += base_test1
+            base_base = cv2.compareHist(hist, hist, 0)
+            base_test1 = cv2.compareHist(hist, hist2, 0)
+            average_img1 += base_test1
 
-        print("color:", color, "img 1:", base_test1)
+            print("color:", color, "img 1:", base_test1)
 
-    eind_result_1 = average_img1 / 3
-    print("Combined simularities img 1: ", eind_result_1)
+        eind_result_1 = average_img1 / 3
+        print("Combined simularities img 1: ", eind_result_1)
 
 # plt.plot(test)
 # plt.title("Image Histogram GFG")
