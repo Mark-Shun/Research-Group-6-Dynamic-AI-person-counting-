8008299375a175afab212de7da858ba84dae826e
nick teeuwen <nickteeuwen21@gmail.com>
2023-06-12 16:02:35 +0200

    generating data for paper

--- test.py
+++ test.py
@@ -10,47 +10,61 @@ import os
 # paper for accuracy of histograms with diffrent color filters
 
 average_img1 = 0
-# average_img2 = 0
-# average_img3 = 0
-# average_img4 = 0
+colors = ("b", "g", "r")
 
-img = cv2.imread("img/output-mens-hoogte(yolov8m-seg)/19-Opname mens hoogte-seg172.jpg")
-for d in os.listdir("C:/Users/nickt/Desktop/Research/img"):
-    print(d)
-    for images in os.listdir(f"C:/Users/nickt/Desktop/Research/img/{d}"):
-        
-        img2 = cv2.imread(f"C:/Users/nickt/Desktop/Research/img/{d}/{images}")
 
-        average_img1 = 0
-        test = hog(
-            img,
-            orientations=8,
-            channel_axis=-1,
-        )
-        test2 = hog(
-        img2,
-        orientations=8,
-        channel_axis=-1,
-        )
 
-        colors = ("b", "g", "r")
-        for i, color in enumerate(colors):
-            hist = cv2.calcHist([img], [i], None, [256], [0, 256])
-            hist[0] = 0
 
-            hist2 = cv2.calcHist([img2], [i], None, [256], [0, 256])
-            hist2[0] = 0
+for im in os.listdir("C:/Users/nickt/Desktop/Research/img/dataset/1"):
+    persoon_id = im[0]
+    img = cv2.imread(f"C:/Users/nickt/Desktop/Research/img/dataset/1/{im}")
+    id_counter = 0
+    id_sum_average = 0
+    counter = 0
+    sum_average = 0
+    
+    all_count = 0
+    all_sum_average = 0
+    all_id_sum_average = 0
 
-            base_base = cv2.compareHist(hist, hist, 0)
-            base_test1 = cv2.compareHist(hist, hist2, 0)
-            average_img1 += base_test1
+    for d in os.listdir("C:/Users/nickt/Desktop/Research/img/organised-data"):        
+        for images in os.listdir(f"C:/Users/nickt/Desktop/Research/img/organised-data/{d}"):
+            
+            img2 = cv2.imread(f"C:/Users/nickt/Desktop/Research/img/organised-data/{d}/{images}")
+            average_img1 = 0
+            
+            for i, color in enumerate(colors):
+                hist = cv2.calcHist([img], [i], None, [256], [0, 256])
+                hist[0] = 0
 
-            # print("color:", color, "img 1:", base_test1)
+                hist2 = cv2.calcHist([img2], [i], None, [256], [0, 256])
+                hist2[0] = 0
 
-        eind_result_1 = average_img1 / 3
-        if(eind_result_1 > 0.75):
-            print(f"{d}/{images}")
-            print("Combined simularities img 1: ", eind_result_1)
+                base_base = cv2.compareHist(hist, hist, 0)
+                base_test1 = cv2.compareHist(hist, hist2, 0)
+                average_img1 = average_img1 + base_test1
+
+            eind_result_1 = average_img1 / 3
+            if(persoon_id == d):
+                id_counter += 1
+                id_sum_average += eind_result_1
+            else:
+                counter += 1
+                sum_average += eind_result_1
+
+                # print(f"{d}/{images}")
+                # print("Combined simularities img 1: ", eind_result_1)
+    result_sum_average = sum_average/counter
+    result_id_sum_average = id_sum_average/id_counter
+    print(f"Not person:{result_sum_average}")        
+    print(f"Right person:{result_id_sum_average}\n")
+
+    all_count += 1
+    all_sum_average += result_sum_average
+    all_id_sum_average += result_id_sum_average
+    
+print(f"ALL Not person:{all_sum_average/all_count}")        
+print(f"ALL Right person:{all_id_sum_average/all_count}")
 print("finished")
 # plt.plot(test)
 # plt.title("Image Histogram GFG")
