2d1364f9c62d5afd448b02ae4b11d304a6aa5e58
coencuppen <74605227+coencuppen@users.noreply.github.com>
2023-06-17 15:44:59 +0200

    creating .pkl and testing threshold values

--- compare.py
+++ compare.py
@@ -3,12 +3,14 @@ from PIL import Image
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt
-
+import pickle
 
 percentageFAST = []
 percentageSIFT = []
 percentageORB = []
 percentageBRIEF = []
+
+person = []
 
 distance_threshold = 40
 lengthMatches = -1
@@ -25,7 +27,7 @@ def computeNumbers(arr, name=''):
     return string, [name, length, sum, average]
 
 
-def FAST(image1, image2, show=True, numberOfMeasurements=None):
+def FAST(image1, image2, show=True, numberOfMeasurements=0, printStats=False):
     text = ''
     data = None
 
@@ -53,6 +55,14 @@ def FAST(image1, image2, show=True, numberOfMeasurements=None):
     filtered_matches = [
         match for match in matches[:lengthMatches] if match.distance < distance_threshold]
 
+    if printStats:
+        print('\nFAST')
+        print('filtered_matches: ', len(filtered_matches))
+        print('matches: ', len(matches))
+        print('keypoints1: ', len(keypoints1))
+        print('keypoints2: ', len(keypoints2))
+        print('\n')
+
     # Calculate match percentage
     if not len(keypoints1):
         match_percentage = 0
@@ -78,7 +88,7 @@ def FAST(image1, image2, show=True, numberOfMeasurements=None):
     return matched_image, text, data
 
 
-def SIFT(image1, image2, show=True, numberOfMeasurements=None):
+def SIFT(image1, image2, show=True, numberOfMeasurements=None, printStats=False):
     text = ''
     data = None
 
@@ -110,8 +120,17 @@ def SIFT(image1, image2, show=True, numberOfMeasurements=None):
 
     # Apply distance threshold to filter out matches
 
+    if printStats:
+        print('\nSIFT')
+        print('filtered_matches: ', len(filtered_matches))
+        print('matches: ', len(matches))
+        print('keypoints1: ', len(keypoints1))
+        print('keypoints2: ', len(keypoints2))
+        print('\n')
+
     # Calculate match percentage
     # print('SIFT: ', len(keypoints1), len(keypoints2), len(filtered_matches))
+
     if not len(keypoints1):
         match_percentage = 0
 
@@ -130,15 +149,14 @@ def SIFT(image1, image2, show=True, numberOfMeasurements=None):
     return matched_image, text, data
 
 
-def ORB(image1, image2, show=True, numberOfMeasurements=None):
+def ORB(image1, image2, show=True, numberOfMeasurements=None, printStats=False):
     text = ''
     data = None
 
     image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
     image2 = cv2.cvtColor(image2, cv2.COLOR_BGR2GRAY)
 
-    orb = cv2.ORB_create(nfeatures=2000, nlevels=8,
-                         scaleFactor=1.2, edgeThreshold=10, patchSize=31)
+    orb = cv2.ORB_create(nfeatures=2000, edgeThreshold=10)
 
     # Detect and compute keypoints and descriptors for both images
     keypoints1, descriptors1 = orb.detectAndCompute(image1, None)
@@ -161,7 +179,16 @@ def ORB(image1, image2, show=True, numberOfMeasurements=None):
     filtered_matches = [
         match for match in matches[:lengthMatches] if match.distance < distance_threshold]
 
+    if printStats:
+        print('\nORB')
+        print('filtered_matches: ', len(filtered_matches))
+        print('matches: ', len(matches))
+        print('keypoints1: ', len(keypoints1))
+        print('keypoints2: ', len(keypoints2))
+        print('\n')
+
     # Calculate match percentage
+
     if not len(keypoints1):
         match_percentage = 0
 
@@ -182,7 +209,7 @@ def ORB(image1, image2, show=True, numberOfMeasurements=None):
     return matched_image, text, data
 
 
-def BRIEF(image1, image2, show=True, numberOfMeasurements=None):
+def BRIEF(image1, image2, show=True, numberOfMeasurements=None, printStats=False):
     text = ''
 
     image1 = cv2.cvtColor(image1, cv2.COLOR_BGR2GRAY)
@@ -262,18 +289,22 @@ def showHistogram(data1=None, data2=None, data3=None):
 
 
 def testAlgorithms(directory: str):
-    global percentageFAST, percentageSIFT, percentageORB
+    global percentageFAST, percentageSIFT, percentageORB, person
     data = []
     for map in os.listdir(directory):
-        percentageFAST.clear()
-        percentageSIFT.clear()
-        percentageORB.clear()
+        # percentageFAST.clear()
+        # percentageSIFT.clear()
+        # percentageORB.clear()
         print('folder: ', map, '\n\n')
         for fileName1 in os.listdir(directory + '/' + map):
             for fileName2 in os.listdir(directory + '/' + map):
                 if fileName1[:3] == fileName2[:3]:
                     continue
                 print(fileName1, fileName2)
+                if map == '1':
+                    person.append(False)
+                else:
+                    person.append(True)
                 image1 = np.array(Image.open(
                     directory + '/' + map + '/' + fileName1))
                 image2 = np.array(Image.open(
@@ -300,11 +331,134 @@ def testAlgorithms(directory: str):
         print(str)
 
 
+def save_image_with_colored_keypoints(image_path, output_path):
+    # Load the image
+    image = cv2.imread(image_path)
+
+    # Convert the image to grayscale
+    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+    # Create a FAST detector object
+    fast = cv2.FastFeatureDetector_create()
+
+    # Detect keypoints using FAST
+    keypoints = fast.detect(gray, None)
+
+    # Generate random colors for keypoints
+    colors = np.random.randint(0, 255, (len(keypoints), 3), dtype=np.uint8)
+
+    # Draw keypoints on the image with different colors
+    image_with_keypoints = np.copy(image)
+    for i, keypoint in enumerate(keypoints):
+        x, y = keypoint.pt
+        cv2.circle(image_with_keypoints, (int(x), int(y)),
+                   6, tuple(colors[i].tolist()), 2)
+
+    # Save the image with keypoints
+    cv2.imwrite(output_path, image_with_keypoints)
+
+    print(f"Image with colored keypoints saved at: {output_path}")
+
+
+def test():
+    image1 = cv2.imread('nick.png')
+    image2 = cv2.imread('mark.png')
+
+    imageFAST, infoFAST, dataFAST = FAST(
+        image1, image2, numberOfMeasurements=1, show=False)
+    imageSIFT, infoSIFT, dataSIFT = SIFT(
+        image1, image2,  numberOfMeasurements=1, show=False)
+    imageORB, infoORB, dataORB = ORB(
+        image1, image2, numberOfMeasurements=1, show=False)
+
+    imageFASTResized = cv2.resize(imageFAST, (640, 240))
+    imageSIFTResized = cv2.resize(imageSIFT, (640, 240))
+    imageORBResized = cv2.resize(imageORB, (640, 240))
+
+    combined_image = np.vstack(
+        (imageFASTResized, imageSIFTResized, imageORBResized))
+    cv2.imshow('foto', combined_image)
+    cv2.waitKey()
+    cv2.destroyAllWindows()
+
+
+def computeThreshold():
+    with open("checkpoint.pkl", "rb") as cp_file:
+        cp = pickle.load(cp_file)
+
+    percentageArrTrue = []
+    percentageArrFalse = []
+    for algorithm in range(3):
+        for threshold in range(0, 100):
+            percentageTrue = 0
+            percentageFalse = 0
+            for index in range(len(cp[0])):
+                if (cp[algorithm][index] > threshold) and cp[3][index]:
+                    percentageTrue += 1
+                if (cp[algorithm][index] < threshold) and cp[3][index]:
+                    percentageTrue -= 1
+                if (cp[algorithm][index] < threshold) and not cp[3][index]:
+                    percentageTrue += 1
+                if (cp[algorithm][index] > threshold) and not cp[3][index]:
+                    percentageTrue -= 1
+
+            percentageArrTrue.append(percentageTrue)
+            percentageArrFalse.append(percentageFalse)
+
+        print(algorithm, '\n\n', percentageArrTrue, '\n\n')
+        percentageArrTrue.clear()
+
+
+def naarNick():
+    with open("checkpoint.pkl", "rb") as cp_file:
+        cp = pickle.load(cp_file)
+
+    arrFAST = []
+    arrSIFT = []
+    arrORB = []
+
+    for i in range(len(cp[3])):
+        arrFAST.append([cp[3][i], cp[0][i]])
+    for i in range(len(cp[3])):
+        arrSIFT.append([cp[3][i], cp[1][i]])
+    for i in range(len(cp[3])):
+        arrORB.append([cp[3][i], cp[2][i]])
+
+    with open("FAST.pkl", "wb") as cp_file:
+        pickle.dump(arrFAST, cp_file)
+    with open("SIFT.pkl", "wb") as cp_file:
+        pickle.dump(arrSIFT, cp_file)
+    with open("ORB.pkl", "wb") as cp_file:
+        pickle.dump(arrORB, cp_file)
+
+
+def testAlgorithms2():
+    global percentageFAST, percentageSIFT, percentageORB, person
+    with open("imgs.pkl", "rb") as cp_file:
+        imgs = pickle.load(cp_file)
+    for compare in imgs:
+        image1 = np.array(Image.open(compare[0]))
+        image2 = np.array(Image.open(compare[1]))
+
+        print(compare)
+        if compare[0][5] == compare[1][5]:
+            person.append(True)
+        else:
+            person.append(False)
+
+        imageFAST, infoFAST, dataFAST = FAST( image1, image2, numberOfMeasurements=1, show=False)
+        imageSIFT, infoSIFT, dataSIFT = SIFT( image1, image2, numberOfMeasurements=1, show=False)
+        imageORB, infoORB, dataORB = ORB( image1, image2, numberOfMeasurements=1, show=False)
+
+        
+
+
 if __name__ == '__main__':
-    import time
-
-    begin = time.time()
-    testAlgorithms('dataset')
-    end = time.time() - begin
-
-    print('time = ', end)
+    # testAlgorithms('dataset')
+    # with open("checkpoint.pkl", "wb") as cp_file:
+    #        pickle.dump([percentageFAST, percentageSIFT, percentageORB, person], cp_file)
+    # naarNick()
+    # computeThreshold()
+    testAlgorithms2()
+    naarNick()
+    
